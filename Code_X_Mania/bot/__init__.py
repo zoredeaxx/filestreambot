7861de3c5f8ea5d1cc6b0ae8c92f3234265e1539
Code X Mania <88939380+Code-X-Mania@users.noreply.github.com>
2021-10-12 15:49:41 +0530

    Create __init__.py

---
+++ Code_X_Mania/bot/__init__.py
@@ -0,0 +1,13 @@
+# This file is a part of FileStreamBot
+# Coding : adarsh @codexmania
+from pyrogram import Client
+from ..vars import Var
+
+StreamBot = Client(
+    session_name='Web Streamer',
+    api_id=Var.API_ID,
+    api_hash=Var.API_HASH,
+    bot_token=Var.BOT_TOKEN,
+    sleep_threshold=Var.SLEEP_THRESHOLD,
+    workers=Var.WORKERS
+)
